be79472e4863a40aaf6d557c6d1ac1edce98a20c
AhmedYasirFarooqui <ahmedfarooqui123@outlook.com>
2021-05-29 14:52:43 +0500

    new commit

---
+++ mi_card_flutter/.dart_tool/flutter_build/0a4df5dc99801ef257c5e2dd049a15a5/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:mi_card_flutter/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
